8b0fdf67a080324143bf83d7cb4c372e3fa52029
Peter Hayglass <peter.hayglass@gmail.com>
2023-12-01 16:45:00 -0600

    code quality

--- day1/day1-2-faster.py
+++ day1/day1-2-faster.py
@@ -1,7 +1,6 @@
 from time import perf_counter
+from typing import Dict, Sequence
 
-
-start = perf_counter()
 
 digit_map = {
     "one": 1, 
@@ -27,40 +26,40 @@ reversed_digit_map = {
 }
 digit_str_len = list(set(len(key) for key in digit_map))
 
-with open("day1/input.txt") as in_file:
-    sum = 0
-    for line in in_file:
-        first = 0
-        second = 0
-        for i, char in enumerate(line):
-            if char.isdigit():
-                first = int(char)
-                break
-            #else:
-            for length in digit_str_len:
-                if line[i:i+length] in digit_map:
-                    first = digit_map[line[i:i+length]]
-                    break
-            if first != 0:
-                break
-        
-        rline = list(reversed(line))
-        for i, char in enumerate(rline):
-            if char.isdigit():
-                second = int(char)
-                break
-            #else:
-            for length in digit_str_len:
-                if ''.join(rline[i:i+length]) in reversed_digit_map:
-                    second = reversed_digit_map[''.join(rline[i:i+length])]
-                    break
-            if second != 0:
-                break
-            
-        line_val = f"{first}{second}"
-        sum += int(line_val)               
-    print(sum)
 
-end = perf_counter()
-run_time = (end - start) * 1000
-print(f"took {run_time}ms")
+def get_first_digit(line: Sequence[str], digit_map: Dict[str, int]) -> int:
+    """Find the first digit in a string or list of chars,
+    either in numerical form or spelled out,
+    and return it as an int."""
+    for i, char in enumerate(line):
+        if char.isdigit():
+            return int(char)
+        for length in digit_str_len:
+            slice = ''.join(line[i:i+length])
+            if slice in digit_map:
+                return digit_map[slice]
+    return 0
+
+
+def main():
+    start = perf_counter()
+
+    with open("day1/input.txt") as in_file:
+        sum = 0
+        for line in in_file:
+            first = get_first_digit(line, digit_map)
+            
+            reversed_line = list(reversed(line))
+            second = get_first_digit(reversed_line, reversed_digit_map)
+
+            line_val = first * 10 + second
+            sum += int(line_val)               
+        print(sum)
+
+    end = perf_counter()
+    run_time = (end - start) * 1000
+    print(f"took {run_time}ms")
+
+
+if __name__ == "__main__":
+    main()
